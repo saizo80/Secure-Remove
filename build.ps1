58885970694b2f63ed2a119cdbb771b8ac70bfd4
Olivier Thornton <oli@simplemail.is>
2023-01-24 13:07:19 -0600

    create build scripts for windows/unix

---
+++ build.ps1
@@ -0,0 +1,4 @@
+dotnet publish -r win-x64 -o Release/win-x64
+dotnet publish -r linux-x64 -o Release/linux-x64
+dotnet publish -r osx-x64 -o Release/osx-x64
+dotnet publish -r osx-arm64 -o Release/osx-arm64
