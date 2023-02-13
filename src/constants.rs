0d1ceeeb851c538dc4bac33ace37d7038b25c250
Olivier Thornton <oli@simplemail.is>
2023-02-13 10:51:33 -0600

    move constants to external file

---
+++ src/constants.rs
@@ -0,0 +1,15 @@
+pub const DEBUG: bool = false;
+pub const VERSION: &'static str = "2.1.0";
+pub const AUTHOR: &'static str = "Olivier Thornton";
+pub const LICENSE: &'static str =
+    "GPLV3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.";
+pub const CHECK_ARGS: [&'static str; 8] = [
+    "-r",
+    "-v",
+    "-p",
+    "--help",
+    "--version",
+    "--verbose",
+    "--recursive",
+    "--passes",
+];
